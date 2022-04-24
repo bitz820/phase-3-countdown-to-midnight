96ee8385215e182d2d51b29931fdf936aafbcb44
Mark <mark.n.bacon.jr@gmail.com>
2022-04-24 19:44:24 -0400

    commit tree 188ca3f20ddb02373b7aa0fc1a39ef8c0e7e80e6

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,16 @@
 #write your code here
 
-def countdown
+def countdown x
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -=1
+    end
+     "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep x
+#    while x>=1
+       sleep(5)
+#        x -= 1
+#    end
 end
